eb91631f2a29c071e00ff7affe5197c588847607
jeezass_dk <xjeezassx@gmail.com>
2022-10-07 01:25:09 +0700

    added user comparsion example

---
+++ Ex004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дима")
+{
+
+    Console.WriteLine("Здаров!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    
+}
